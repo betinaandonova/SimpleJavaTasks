38758c5f4300059bb24c2ba8db4f4cca52d1539b
Betina Andonova <betina_andonova@abv.bg>
2026-03-27 16:21:27 +0200

    Initial commit

---
+++ firstTask/unevenNumbers/src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main()
+{
+   int n=300;
+   
+    for(int i=0; i<=n; i++)
+    {
+       if(i%2 != 0)
+       {
+           System.out.println(i);
+       }
+   }
+}
